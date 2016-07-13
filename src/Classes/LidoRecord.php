62f739c35a5ea515b7880c8e59a8a466d29e206f
Frank Morgner <morgnerf@ub.uni-leipzig.de>
2016-07-13 13:26:23 +0200

    refs #8017 add wrapper LidoRecord class to call cli with native LiberoRecord of NatLibFi means without -s|--source parameter. thx miku for testing and revealing some programme weakness

---
+++ src/Classes/LidoRecord.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Lido - Record extending LidoRecord of NatLibFi class for LIDO Records
+ *
+ * Copyright (C) 2016 Leipzig University Library
+ *
+ * @author Frank Morgner <morgnerf@ub.uni-leipzig.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace LidoCli\Classes;
+
+use \LidoCli\Ndl\LidoRecord as NativeLidoRecord;
+
+class LidoRecord extends NativeLidoRecord implements LidoRecordInterface
+{
+    /**
+     * Collector for array to import in Solr
+     *
+     * @return array    Returns array to import in Solr
+     * @access public
+     */
+    public function toSolrArray()
+    {
+        return parent::toSolrArray();
+    }
+
+    /**
+     * Collector for json to import in Solr
+     *
+     * @return string    Returns json to import in Solr
+     * @access public
+     */
+    public function toSolrJson()
+    {
+        return json_encode($this->toSolrArray(), JSON_UNESCAPED_UNICODE) . "\n";
+    }
+}
